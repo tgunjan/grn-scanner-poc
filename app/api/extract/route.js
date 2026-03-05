4a158e1b4b423f1214e9b857d33376e569f14958
tgunjan <tgunjan@gmail.com>
2026-03-05 17:21:26 +0530

    Update route.js

--- app/api/extract/route.js
+++ app/api/extract/route.js
@@ -114,7 +114,7 @@ export async function POST(request) {
 
     const response = await client.messages.create({
       //model: "claude-sonnet-4-20250514",
-      model: "claude-sonnet-4-5-20250929",
+      model: "claude-opus-4-0-20250514",
       max_tokens: 2000,
       messages: [
         {
